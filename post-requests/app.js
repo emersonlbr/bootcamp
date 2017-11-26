e449ba3f554f1b82e765015ea6dbe0e596c2ee14
emersonlbr <emersonlbr@icloud.com>
2017-11-26 00:55:15 -0200

    intro to post requests

--- post-requests/app.js
+++ post-requests/app.js
@@ -1,2 +1,33 @@
 var express = require("express");
+var app = express();
 
+// makes the post an object
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+
+
+var friends = ["Daniel", "Bruno", "Matheus", "Jorge", "Marcos"];
+
+app.get("/", function(req, res) {
+  res.render('home');
+});
+
+app.get("/friends", function(req, res) {
+  res.render("friends", {friends: friends});
+});
+
+// post routes are used when we want to send data
+app.post("/addfriend", function(req, res) {
+  // cathing object on name="newfrend" on home.ejs
+  var newFriend = req.body.newfriend;
+  // adding the friend to the array
+  friends.push(newFriend);
+  // res.send("you have reached the post route!");
+  res.redirect("/friends");
+});
+
+app.listen(3000, function(){
+  console.log("Server is runing on port 3000");
+});
